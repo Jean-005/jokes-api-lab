464e726163d5960b572173cfe8b56228362db599
Jean <jean-ata@hotmail.co.uk>
2024-03-27 14:42:28 +0000

    updated container file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import JokesContainer from './containers/JokesContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
-        
+      <JokesContainer /> 
+      
       </header>
     </div>
   );
--- src/containers/JokesContainer.js
+++ src/containers/JokesContainer.js
@@ -1,4 +1,20 @@
+import { useState, useEffect } from "react";
+
 const JokesContainer = () => {
+    const [jokes, setJokes] = useState([]);
+
+
+    const loadData = async () => {
+        const response = await fetch("https://raw.githubusercontent.com/15Dkatz/official_joke_api/blob/master/jokes/index.json");
+        const jsonData = await response.json();
+        setJokes(jsonData);
+        console.log(jsonData);
+    }
+
+    useEffect(() => {
+        loadData();
+    }, []);
+    
     return (  
 
 
